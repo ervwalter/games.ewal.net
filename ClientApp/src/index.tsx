f506b2586fa24e11a242be26a355260be7502016
Erv Walter <erv@ewal.net>
2018-06-06 06:18:39 -0500

    remove serviceWorker

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "~/components/App";
-import registerServiceWorker from "~/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+// registerServiceWorker();
