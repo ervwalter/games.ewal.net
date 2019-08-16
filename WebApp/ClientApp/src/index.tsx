c2ca1a09da59d2d2061bd9bb024c8b80db3fb486
Erv Walter <erv@ewal.net>
2019-08-16 09:03:32 -0500

    refactor analytics

--- WebApp/ClientApp/src/index.tsx
+++ WebApp/ClientApp/src/index.tsx
@@ -5,7 +5,6 @@ import "core-js";
 import "regenerator-runtime/runtime";
 
 import "./index.scss";
-import "./utils/Analytics";
 
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
@@ -13,6 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import UnsupportedBrowser from "./components/UnsupportedBrowser";
+import tracker from "./utils/Analytics";
 import analyticsTracker from "./utils/Analytics";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") || undefined;
@@ -20,6 +20,21 @@ const rootElement = document.getElementById("root");
 
 const isSupported = typeof Proxy !== "undefined" && typeof Symbol !== "undefined" && true;
 const App = React.lazy(() => import(/* webpackChunkName: "app" */ "./components/App"));
+
+let commitHash = (process && process.env && process.env.REACT_APP_COMMIT) || undefined;
+let buildId = (process && process.env && process.env.REACT_APP_BUILD_ID) || undefined;
+let version;
+if (commitHash && buildId) {
+	version = `trendweight-${buildId}-${commitHash.substr(0, 7)}`;
+} else {
+	version = "trendweight-dev";
+}
+
+tracker.init({
+	gaugesIdentifier: "5c94e5859482cb34c9ee7690",
+	ostrioIdendifier: "68bCGRQxE8hcjya6P",
+	sentryDSN: "https://fa2b74a3356e44ae949f9bf9938fdbc0@sentry.io/1531751"
+});
 
 if (isSupported) {
 	ReactDOM.render(
--- WebApp/ClientApp/src/utils/Analytics.ts
+++ WebApp/ClientApp/src/utils/Analytics.ts
@@ -1,60 +1,80 @@
 import * as Sentry from "@sentry/browser";
 import OstrioAnalytics from "ostrio-analytics";
 
-const log =
-	console.info.bind(console) ||
-	console.log.bind(console) ||
-	function() {
-		return;
-	};
-
-const DNT = navigator.doNotTrack === "1";
-if (DNT) {
-	log("I respect your decision to be not tracked. Analytics & error tracking have been");
-	log("disabled for you. Learn more about DNT: https://en.wikipedia.org/wiki/Do_Not_Track");
-	log(`Sincerely, ${window.location.origin}`);
-}
-
-if (!DNT) {
-	Sentry.init({ dsn: "https://fa2b74a3356e44ae949f9bf9938fdbc0@sentry.io/1531751" });
-}
-
-let ostrioTracker: { track(): void };
-
 class Tracker {
-	public track = () => {
-		// skip tracking if DNT is enabled
-		if (DNT) {
+	private ostrioIdentifier?: string;
+	private gaguesIdentifier?: string;
+	private isInitialized: boolean = false;
+	private ostrioTracker?: { track(): void };
+	private DNT: boolean = navigator.doNotTrack === "1";
+
+	public init(options: { gaugesIdentifier?: string; ostrioIdendifier?: string; sentryDSN?: string; sentryRelease?: string }) {
+		if (this.DNT) {
+			this.log("I respect your decision to be not tracked. Analytics & error tracking have been");
+			this.log("disabled. Learn more about DNT: https://en.wikipedia.org/wiki/Do_Not_Track");
+			this.log(`Sincerely, ${window.location.origin}`);
+			this.isInitialized = true;
 			return;
 		}
 
-		// trigger ostr.io tracker
-		if (!ostrioTracker) {
-			ostrioTracker = new OstrioAnalytics("68bCGRQxE8hcjya6P", false);
-		}
-		ostrioTracker.track();
+		this.ostrioIdentifier = options.ostrioIdendifier;
+		this.gaguesIdentifier = options.gaugesIdentifier;
 
-		// trigger gaug.es tracker
-		const w = window as any;
-		const gauges = w._gauges;
-		if (!gauges) {
-			// initialize gaug.es tracker which auto tracks once when loaded
-			w._gauges = w._gauges || [];
-			(() => {
-				const t = document.createElement("script");
-				t.type = "text/javascript";
-				t.async = true;
-				t.id = "gauges-tracker";
-				t.setAttribute("data-site-id", "5c94e5859482cb34c9ee7690");
-				t.setAttribute("data-track-path", "https://track.gaug.es/track.gif");
-				t.src = "https://d2fuc4clr7gvcn.cloudfront.net/track.js";
-				const s = document.getElementsByTagName("script")[0];
-				s && s.parentNode && s.parentNode.insertBefore(t, s);
-			})();
-		} else {
-			gauges.push(["track"]);
+		// initialize Sentry.io
+		if (!this.DNT && options.sentryDSN) {
+			if (options.sentryRelease) {
+				Sentry.init({ dsn: options.sentryDSN, release: options.sentryRelease });
+			} else {
+				Sentry.init({ dsn: options.sentryDSN });
+			}
+		}
+
+		this.isInitialized = true;
+	}
+	public track = () => {
+		// skip tracking if DNT is enabled, or if things haven't been intiaized
+		if (this.DNT || !this.isInitialized) {
+			return;
+		}
+
+		if (this.ostrioIdentifier) {
+			// trigger ostr.io tracker
+			if (!this.ostrioTracker) {
+				this.ostrioTracker = new OstrioAnalytics(this.ostrioIdentifier, false);
+			}
+			this.ostrioTracker!.track();
+		}
+
+		if (this.gaguesIdentifier) {
+			// trigger gaug.es tracker
+			const w = window as any;
+			const gauges = w._gauges;
+			if (!gauges) {
+				// initialize gaug.es tracker which auto tracks once when loaded
+				w._gauges = w._gauges || [];
+				(() => {
+					const t = document.createElement("script");
+					t.type = "text/javascript";
+					t.async = true;
+					t.id = "gauges-tracker";
+					t.setAttribute("data-site-id", this.gaguesIdentifier);
+					t.setAttribute("data-track-path", "https://track.gaug.es/track.gif");
+					t.src = "https://d2fuc4clr7gvcn.cloudfront.net/track.js";
+					const s = document.getElementsByTagName("script")[0];
+					s && s.parentNode && s.parentNode.insertBefore(t, s);
+				})();
+			} else {
+				gauges.push(["track"]);
+			}
 		}
 	};
+
+	private log = (message: string) =>
+		console.info.bind(console) ||
+		console.log.bind(console) ||
+		function(message: string) {
+			return;
+		};
 }
 
 export default new Tracker();
