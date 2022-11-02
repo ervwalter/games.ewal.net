867df4ce874794d0bcf1ed11a5b8ad4dc1e81713
Erv Walter <erv@ewal.net>
2022-11-02 05:33:28 +0000

    debugging output

--- frontend/lib/games-data.ts
+++ frontend/lib/games-data.ts
@@ -1,11 +1,15 @@
 import "server-only";
 
 export async function getPlays() {
-  const plays = await fetch(
+  console.log("before fetch");
+  const _plays = await fetch(
     "https://ewalgamescache.blob.core.windows.net/gamescache/plays-ervwalter.json",
     //    "https://jsonplaceholder.typicode.com/comments",
     { cache: "no-store" }
     // { next: { revalidate: 10 } }
   );
-  return await plays.json();
+  console.log("after fetch/before .text()");
+  const plays = await _plays.text();
+  console.log("after .text()");
+  return plays;
 }
