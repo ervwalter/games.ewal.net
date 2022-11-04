039c9b15ac75831bbb4e4d77d5de312d41fe4fdf
Erv Walter <erv@ewal.net>
2022-11-04 01:29:00 +0000

    move top level redirect to next.js.config

--- frontend/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/overview");
-  return null;
-}
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/overview",
+        permanent: false,
+      },
+    ];
+  },
   experimental: {
     appDir: true,
     enableUndici: true,
