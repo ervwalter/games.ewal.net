6e7c5a137777363e605bd12dd2c2c9185b6857c6
Erv Walter <erv@ewal.net>
2022-11-08 04:35:21 +0000

    layout ui tweaks

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,7 +9,7 @@ const glyphicons = localFont({ variable: "--font-glyphicons", src: "../fonts/gly
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={clsx(inter.variable, glyphicons.variable, "h-full md:overflow-y-scroll")}>
+    <html lang="en" className={clsx(inter.variable, glyphicons.variable, "h-full")}>
       <head>
         <title>Board Games</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Sidebar />
         <div className="flex flex-1 flex-col md:pl-52 lg:pl-64">
           <main className="flex-1">
-            <div className="max-w-6xl border-gray-200 px-4 py-6 sm:px-6 md:border-r md:bg-gray-50 md:px-8">
+            <div className="min-h-screen max-w-6xl border-gray-200 px-4 py-6 sm:px-6 md:border-r md:bg-gray-50 md:px-8">
               <div>{children}</div>
             </div>
           </main>
