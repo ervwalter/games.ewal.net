2d900725e423980f1e27e3d963a3206a1c89ab1c
Erv Walter <erv@ewal.net>
2022-11-09 23:43:35 +0000

    insight ui improvements

--- frontend/app/insights/charts.tsx
+++ frontend/app/insights/charts.tsx
@@ -1,127 +1,74 @@
-"use client";
+import { schemeBlues, schemeGreens, schemePurples, schemeReds } from "d3-scale-chromatic";
+import { Insights } from "lib/insights";
+import { sumBy, take } from "lodash";
+import { HorizontalBarChart, PieChart } from "../../components/charts";
 
-import { BarCommonProps, ResponsiveBar } from "@nivo/bar";
-import { CommonPieProps, ResponsivePie } from "@nivo/pie";
+export default function InsightsCharts({ insights }: { insights: Insights }) {
+  const locationColors = schemeBlues[9];
+  const dayColors = schemeGreens[8];
+  const playerCountColors = schemePurples[9];
+  const playersColors = schemeReds[9];
 
-type Datum = {
-  id: string;
-  value: number;
-};
-
-interface HorizontalBarChartProps<T extends Datum> {
-  data: T[];
-  yLabel: string;
-  colors: BarCommonProps<T>["colors"];
-  labels?: boolean;
-}
-
-export function HorizontalBarChart<T extends Datum>({
-  data,
-  yLabel,
-  colors,
-  labels = false,
-}: HorizontalBarChartProps<T>) {
   return (
-    <ResponsiveBar
-      data={data}
-      keys={["value"]}
-      indexBy="id"
-      tooltipLabel={({ indexValue }) => indexValue as string}
-      margin={{ top: 0, right: 25, bottom: 20, left: 75 }}
-      padding={0.25}
-      groupMode="grouped"
-      layout="horizontal"
-      valueScale={{ type: "linear" }}
-      // indexScale={{ type: "band", round: true }}
-      valueFormat=" >-"
-      colors={colors}
-      borderColor={{
-        from: "color",
-        modifiers: [["brighter", 2]],
-      }}
-      borderWidth={1}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        tickValues: 5,
-      }}
-      axisLeft={{
-        tickSize: 0,
-      }}
-      enableGridY={false}
-      enableLabel={labels}
-      legends={[]}
-      role="application"
-      ariaLabel={`${yLabel} bar chart`}
-      barAriaLabel={function (e) {
-        return `${e.id}: ${e.formattedValue} in ${yLabel}: ${e.indexValue}`;
-      }}
-    />
+    <>
+      <div className="flex flex-row flex-wrap items-center justify-start gap-4">
+        <div className="flex w-full max-w-[350px] flex-col  ">
+          <div className="text-center font-semibold">Plays by Location</div>
+          <div className="aspect-w-2 aspect-h-1">
+            <PieChart colors={locationColors.slice(1)} data={getData(insights.locations, 7, "location")}></PieChart>
+          </div>
+        </div>
+        <div className="flex w-full max-w-[350px] flex-col  ">
+          <div className="text-center font-semibold">Plays by Day of Week</div>
+          <div className="aspect-w-2 aspect-h-1">
+            <PieChart colors={dayColors.slice(1)} data={getData(insights.daysOfTheWeek, 8, "day")}></PieChart>
+          </div>
+        </div>
+        <div className="flex w-full max-w-[350px] flex-col  ">
+          <div className="text-center font-semibold">Plays by Player Count</div>
+          <div className="aspect-w-2 aspect-h-1">
+            <PieChart
+              colors={playerCountColors.slice(2)}
+              data={getData(insights.playerCounts, 6, "players", "6+ players")}></PieChart>
+          </div>
+        </div>
+        <div className="flex w-full max-w-[600px] flex-col ">
+          <div className="pb-2 text-center font-semibold">Most Frequent Players</div>
+          <div className="h-[250px]">
+            <HorizontalBarChart
+              colors={playersColors.slice(3)}
+              data={getData(
+                take(
+                  insights.statsPerPlayer.filter((p) => p.playerName !== "Erv"),
+                  10
+                ).reverse(),
+                10,
+                "playerName"
+              )}
+              axisLabel={"Locations"}
+              showLabels={false}></HorizontalBarChart>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
-interface PieProps<T extends Datum> {
-  data: T[];
-  colors: CommonPieProps<T>["colors"];
-  labels?: boolean;
+function getData<T extends { plays: number }>(
+  sourceData: T[],
+  max: number,
+  key: keyof Omit<T, "plays">,
+  otherLabel = "Other"
+) {
+  const selected: T[] = take(sourceData, max - 1);
+  if (sourceData.length == max) {
+    selected.push(sourceData[max - 1]);
+  }
+  const data = selected.map((datum) => ({ id: datum[key] as string, value: datum.plays }));
+
+  if (sourceData.length > max) {
+    const other = sumBy(sourceData.slice(max - 1), "plays");
+    data.push({ id: otherLabel, value: other });
+  }
+  return data;
 }
-
-export function Pie<T extends Datum>({ data, colors, labels = false }: PieProps<T>) {
-  return (
-    <ResponsivePie
-      data={data}
-      margin={{
-        top: 30,
-        right: 60,
-        bottom: 30,
-        left: 60,
-      }}
-      innerRadius={0.7}
-      padAngle={0}
-      cornerRadius={0}
-      activeOuterRadiusOffset={8}
-      colors={colors}
-      borderWidth={1}
-      borderColor={{
-        from: "color",
-        modifiers: [["darker", 0.5]],
-      }}
-      enableArcLinkLabels={true}
-      enableArcLabels={labels}
-      arcLabel="id"
-      arcLabelsRadiusOffset={0.6}
-      arcLinkLabelsSkipAngle={5}
-      arcLinkLabelsColor="#ccc"
-      arcLinkLabelsStraightLength={8}
-      arcLinkLabelsDiagonalLength={20}
-      // legends={[
-      //   {
-      //     anchor: "right",
-      //     direction: "column",
-      //     justify: false,
-      //     translateX: 0,
-      //     translateY: 0,
-      //     itemWidth: 120,
-      //     itemHeight: 20,
-      //     itemsSpacing: 0,
-      //     symbolSize: 10,
-      //     symbolBorderColor: "#333333",
-      //     symbolBorderWidth: 0.5,
-      //     itemDirection: "left-to-right", // symbolShape: "circle",
-      //   },
-      // ]}
-    />
-  );
-}
-
-// export default function LocationsChart({ locations }: LocationsChartProps) {
-//   const colors = schemePuBu[9].slice();
-//   colors.reverse();
-//   // colors = colors.slice(1);
-//   const data = ;
-
-//   return <Pie colors={colors} data={getData(locations, 8, "location")}></Pie>;
-// }
--- frontend/app/insights/page.tsx
+++ frontend/app/insights/page.tsx
@@ -1,15 +1,17 @@
 import { Blurb } from "components/blurb";
 import { getInsights } from "lib/insights";
-import InsightsCharts from "./insights-charts";
+import InsightsCharts from "./charts";
+import PlayersInsights from "./players";
 
 export default async function InsightsPage() {
   const insights = await getInsights();
   return (
     <div className="flex flex-1 flex-col space-y-4">
       <Blurb />
-      <h2 className="text-2xl font-semibold">Insights</h2>
+      <h2 className="text-2xl font-semibold">Patterns and Statistics</h2>
       <InsightsCharts insights={insights} />
-      {/* <PlayersInsights insights={insights} /> */}
+      <h2 className="text-2xl font-semibold">Player Stats</h2>
+      <PlayersInsights insights={insights} />
     </div>
   );
 }
