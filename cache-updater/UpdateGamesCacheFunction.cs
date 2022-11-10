4a8fc07f02458625dba54b38cf3579e01eb4c6c1
Erv Walter <erv@ewal.net>
2022-11-10 05:31:50 +0000

    output exceptions

--- cache-updater/UpdateGamesCacheFunction.cs
+++ cache-updater/UpdateGamesCacheFunction.cs
@@ -7,31 +7,39 @@ using System.Threading.Tasks;
 
 namespace GamesCacheUpdater
 {
-    public static class UpdateGamesCacheFunction
-    {
-        [FunctionName("UpdateGamesCache")]
-        public static async Task Run([TimerTrigger("0 */15 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
-        {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-            log.LogInformation($"Updating Cache at {DateTime.Now}");
-            var username = config["bgg_username"];
-            var password = config["bgg_password"];
-            var storage = config["cache_storage"];
-            var updater = new CacheUpdater(log);
-            await updater.InitializeAsync(storage, username, password);
-            await updater.DownloadPlaysAsync();
-            await updater.DownloadCollectionAsync();
-            await updater.DownloadTopTenAsync();
-            await updater.LoadCachedGameDetailsAsync();
-            await updater.DownloadUpdatedGameDetailsAsync();
-            updater.ProcessPlays();
-            updater.ProcessCollection();
-            updater.ProcessTopTen();
-            updater.GenerateStats();
-            await updater.SaveEverythingAsync();        }
-    }
-}
+	public static class UpdateGamesCacheFunction
+	{
+		[FunctionName("UpdateGamesCache")]
+		public static async Task Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+		{
+			try
+			{
+				var config = new ConfigurationBuilder()
+					.SetBasePath(context.FunctionAppDirectory)
+					.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+					.AddEnvironmentVariables()
+					.Build();
+				log.LogInformation($"Updating Cache at {DateTime.Now}");
+				var username = config["bgg_username"];
+				var password = config["bgg_password"];
+				var storage = config["cache_storage"];
+				var updater = new CacheUpdater(log);
+				await updater.InitializeAsync(storage, username, password);
+				await updater.DownloadPlaysAsync();
+				await updater.DownloadCollectionAsync();
+				await updater.DownloadTopTenAsync();
+				await updater.LoadCachedGameDetailsAsync();
+				await updater.DownloadUpdatedGameDetailsAsync();
+				updater.ProcessPlays();
+				updater.ProcessCollection();
+				updater.ProcessTopTen();
+				updater.GenerateStats();
+				await updater.SaveEverythingAsync();
+			}
+			catch (Exception ex)
+			{
+				log.LogError(ex.ToString());
+			}
+
+		}
+	}
