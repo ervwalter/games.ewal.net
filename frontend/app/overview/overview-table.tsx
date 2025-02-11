a6ba8118937a649ac5e90206da90a74d05900fbf
Erv Walter <erv@ewal.net>
2025-02-11 07:52:07 -0600

    tweak stats

--- frontend/app/overview/overview-table.tsx
+++ frontend/app/overview/overview-table.tsx
@@ -8,15 +8,17 @@ export default async function OverviewTable() {
 
   return (
     <div className="flex flex-wrap">
-      <div className="mb-4 w-full flex-none pr-2 md:mb-0 md:w-1/3">
+      <div className={`mb-4 w-full flex-none pr-2 md:mb-0 ${playsThisYear.numberOfPlays > 0 ? 'md:w-1/3' : 'md:w-1/2'} lg:w-1/3`}>
         <CollectionStatsBlock stats={collection} />
       </div>
-      <div className="w-1/2 flex-none pr-2 md:w-1/3">
+      <div className={`w-1/2 flex-none pr-2 ${playsThisYear.numberOfPlays > 0 ? 'md:w-1/3' : 'md:w-1/2'} lg:w-1/3`}>
         <PlaysStatsBlock stats={playsAllTime} />
       </div>
-      <div className="w-1/2 flex-none pr-2 md:w-1/3">
-        <YearStatsBlock stats={playsThisYear} year={thisYear} />
-      </div>
+      {playsThisYear.numberOfPlays > 0 && (
+        <div className="w-1/2 flex-none pr-2 md:w-1/3 lg:w-1/3">
+          <YearStatsBlock stats={playsThisYear} year={thisYear} />
+        </div>
+      )}
     </div>
   );
 }
